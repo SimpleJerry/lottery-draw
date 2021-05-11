cb2aeac353ac1ab2451ee24af1083cbe287a54bc
Simple Jerry <925209421@qq.com>
2021-05-11 17:26:54 +0800

    更换启动类位置：在启动类头部增加@ComponentScan注解，以重新规定其扫描范围

--- src/main/java/com/jerry/lottery_draw/config/LotteryDrawApplication.java
+++ src/main/java/com/jerry/lottery_draw/config/LotteryDrawApplication.java
@@ -1,11 +1,13 @@
-package com.jerry.lottery_draw;
+package com.jerry.lottery_draw.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.jerry")
 @SpringBootApplication
 public class LotteryDrawApplication {
 
