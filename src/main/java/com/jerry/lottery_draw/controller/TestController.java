4423bdd23065fe84d7aa38dd1f1196d6b7cdacc8
Simple Jerry <925209421@qq.com>
2021-05-12 14:16:09 +0800

    Mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/jerry/lottery_draw/controller/TestController.java
+++ src/main/java/com/jerry/lottery_draw/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.jerry.lottery_draw.controller;
 
+import com.jerry.lottery_draw.domain.Test;
+import com.jerry.lottery_draw.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
 
     /**
      * GET, POST, PUT, DELETE
@@ -21,5 +29,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
---
+++ src/main/java/com/jerry/lottery_draw/service/TestService.java
@@ -0,0 +1,21 @@
+package com.jerry.lottery_draw.service;
+
+import com.jerry.lottery_draw.domain.Test;
+import com.jerry.lottery_draw.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.selectByExample(null);
+    }
+}
+
+
