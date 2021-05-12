002e51bcae88c058562da0f034ee7ccb101810a3
Simple Jerry <925209421@qq.com>
2021-05-12 14:14:08 +0800

    补充：添加注解@MapperScan，将Mapper交由Spring管理

--- src/main/java/com/jerry/lottery_draw/config/LotteryDrawApplication.java
+++ src/main/java/com/jerry/lottery_draw/config/LotteryDrawApplication.java
@@ -1,5 +1,6 @@
 package com.jerry.lottery_draw.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @ComponentScan("com.jerry")
 @SpringBootApplication
+@MapperScan("com.jerry.lottery_draw.mapper")
 public class LotteryDrawApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(LotteryDrawApplication.class);
