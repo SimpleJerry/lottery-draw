855c959223340b1b27dc73fbaecb05c760ac9b3c
Simple Jerry <925209421@qq.com>
2021-06-18 15:22:31 +0800

    Vue多环境配置

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,9 +4,20 @@ import router from "./router";
 import store from "./store";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import * as Icons from "@ant-design/icons-vue"; // 图标库
 
-createApp(App)
+const app = createApp(App);
+app
   .use(store)
   .use(router)
   .use(Antd)
   .mount("#app");
+
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+  app.component(i, icons[i]);
+}
+
+console.log("环境：", process.env.NODE_ENV);
+console.log("服务端：", process.env.VUE_APP_SERVER);
