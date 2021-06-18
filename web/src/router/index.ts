7d9500b76fefc4a5f7dd4dc87ce155b00a5ad6c9
Simple Jerry <925209421@qq.com>
2021-06-18 17:17:09 +0800

    增加测试路由

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/home.vue";
+import About from "../views/about.vue";
+import Test from "../views/test.vue";
+import Award from "../views/admin/admin-award.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +11,19 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
+    path: "/test",
+    name: "Test",
+    component: Test,
+  },
+  {
+    path: "/award",
+    name: "AdminAward",
+    component: Award,
+  },
+  {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
